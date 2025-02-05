872da41e59de13c118ecaa45c908649c39c6a939
pancake0003 <songyuemmm@gmail.com>
2025-02-05 16:59:52 -0500

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,46 @@
+const express = require ('expres');
+const path = require ('path');
+
+const app = express();
+
+app.use((req, res, next) => {
+	console.log(`Incoming request: $req.method} ${req.path}`);
+	next;
+});
+
+function workingHours (req, res, next){
+	if(req.path === 'Yuemeng_Song_Resume.pdf') {
+		console.log ("Accessing resume: Allowed");
+		return next();
+	}
+
+	const currentTime = new Date();
+	const currentHour = currentTime.getHours();
+
+	const normalBusinessHours = {
+		open: 15, 
+		close: 3,
+	};
+
+	if (currentHour >= normalBusinessHours.open || currentHour < normalBusinessHours.close) {
+		console.log("Within business hours: Allowed");
+		next();
+	} else {
+		console.log("Outside business hours: Redirecting to denied.html);
+		res.sendFile(path.join(__dirname, 'public', 'denied.html'));
+	}
+}
+app.use (express.static(path.join(__dirname, 'public')));
+
+app.use(workingHours);
+
+app.get('/Yuemeng_Song_Resume.pdf', (req, res) => {
+	res.sendFIle(path.join(__dirname, 'public', 'Yuemeng_Song_Resume.pdf'));
+});
+
+app.get('/', (req, rers) => {
+	res.sendFIle(path.join(__dirname, 'public', 'index.html'));
+});
+
+module.exports = app;
+
