ad8f8db399215699e6386153598eb1710005ce34
pancake0003 <songyuemmm@gmail.com>
2025-02-05 17:06:58 -0500

    typos

--- api/index.js
+++ api/index.js
@@ -1,46 +1,52 @@
-const express = require ('express');
-const path = require ('path');
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.use((req, res, next) => {
-	console.log(`Incoming request: $req.method} ${req.path}`);
-	next;
+  console.log(`Incoming request: ${req.method} ${req.path}`);
+  next();
 });
 
-function workingHours (req, res, next){
-	if(req.path === 'Yuemeng_Song_Resume.pdf') {
-		console.log ("Accessing resume: Allowed");
-		return next();
-	}
+function workingHours(req, res, next) {
+  if (req.path === '/Yuemeng_Song_Resume.pdf') {
+    console.log("Accessing resume: Allowed");
+    return next();
+  }
 
-	const currentTime = new Date();
-	const currentHour = currentTime.getHours();
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
 
-	const normalBusinessHours = {
-		open: 15, 
-		close: 3,
-	};
+  const normalBusinessHours = {
+    open: 15,
+    close: 3,
+  };
 
-	if (currentHour >= normalBusinessHours.open || currentHour < normalBusinessHours.close) {
-		console.log("Within business hours: Allowed");
-		next();
-	} else {
-		console.log("Outside business hours: Redirecting to denied.html");
-		res.sendFile(path.join(__dirname, 'public', 'denied.html'));
-	}
+  if (currentHour >= normalBusinessHours.open || currentHour < normalBusinessHours.close) {
+    console.log("Within business hours: Allowed");
+    next();
+  } else {
+    console.log("Outside business hours: Redirecting to denied.html");
+    res.sendFile(path.resolve('public', 'denied.html'));
+  }
 }
-app.use (express.static(path.join(__dirname, 'public')));
 
 app.use(workingHours);
-
-app.get('/Yuemeng_Song_Resume.pdf', (req, res) => {
-	res.sendFIle(path.join(__dirname, 'public', 'Yuemeng_Song_Resume.pdf'));
-});
+app.use(express.static(path.resolve('public')));
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  console.log("Serving index.html");
+  res.sendFile(path.resolve('public', 'index.html'));
+});
+
+app.get('/api', (req, res) => {
+  console.log("API check successful");
+  res.json({ message: 'API is working!' });
+});
+
+app.use((req, res) => {
+  console.log(`404: ${req.path} not found`);
+  res.status(404).send('Not Found');
 });
 
 module.exports = app;
-
